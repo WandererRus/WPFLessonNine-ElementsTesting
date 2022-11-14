84e362c9c8ac3221af3006ef4b7b074402b069a5
Fastov Ilya Olegovich <iluxailuxa@gmail.com>
2022-11-14 21:31:37 +0300

    123456

--- WPFLessonNine-ElementsTesting/MainWindow.xaml.cs
+++ WPFLessonNine-ElementsTesting/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -23,6 +24,7 @@ namespace WPFLessonNine_ElementsTesting
         public MainWindow()
         {
             InitializeComponent();
+            groupB.Height = 500;
         }
 
         private void cb_firstItem_Click(object sender, RoutedEventArgs e)
@@ -44,6 +46,25 @@ namespace WPFLessonNine_ElementsTesting
             tree_sample3.Items.Add(((ListBoxItem)lb_sample.Items[3]).Content);
             tree_sample3.Items.Add(((ListBoxItem)lb_sample.Items[4]).Content);
             ((TreeViewItem)tree_sample.Items[1]).Items.Add(tree_sample3);
+        }
+
+        private void btn_start_Click(object sender, RoutedEventArgs e)
+        {
+            for (double i = pb.Minimum; i < pb.Maximum; i++)
+            {            
+                pb.Value = i;
+            }
+            /*for (double i = pb.Maximum; i > pb.Minimum; i--)
+            {
+                forma.Height = i;
+                pb.Value = i;
+                Thread.Sleep(15);
+            }*/
+        }
+
+        private void sli_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            groupB.Height = 100 ;
         }
     }
 }
